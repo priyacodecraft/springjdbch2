425e3c4121b8f6669aa490d7388aa0835bb5a4d4
priyacodecraft <lakshmipriya.g@gmail.com>
2023-01-22 11:44:07 -0600

    first commit

---
+++ src/test/java/com/example/relationaldatabase/RelationaldatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.relationaldatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelationaldatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
